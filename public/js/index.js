33beb2fdf8181fe969452dcda86f879c1301ba73
wslider <wslider2000@gmail.com>
2025-10-14 14:58:27 -0400

    nodemon added to package.json functions for looping through quotes (setInterval and setTimeout) added to index.js

---
+++ public/js/index.js
@@ -0,0 +1,45 @@
+"use strict"
+
+const elements = {
+    quote: document.getElementById("quote"),
+    author: document.getElementById("author"),
+};
+
+const quotes = [
+    {
+        quote: "All hands! Abandon ship!",
+        author: "Captain Picard",
+    },
+
+    {
+        quote: "Doh!",
+        author: "Homer Simpson",
+    },
+
+    {
+        quote: "The Internet is the first thing that humanity has built that humanity doesn't understand, the largest experiment in anarchy that we have ever had.",
+        author: "Eric Schmidt",
+    },
+
+    {
+        quote: "Birds don't fly, they just flap their wings.",
+        author: "J Chervenak",
+    }
+
+
+]
+
+function loopThroughQuotes() {
+    let quoteIndex = 0;
+    setInterval(() => {
+        if (quoteIndex < quotes.length) {
+            elements.quote.textContent = quotes[quoteIndex].quote;
+            elements.author.textContent = quotes[quoteIndex].author;
+            quoteIndex++;
+        } else {
+            quoteIndex = 0;
+        }
+    }, 3000);
+}
+
+setTimeout(loopThroughQuotes, 3000);
