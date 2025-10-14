2789cc75078c73b6a1549826c6c06183b57b18e9
wslider <wslider2000@gmail.com>
2025-10-14 15:47:58 -0400

    added async funtion to all Unsplash API and get random image

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,23 @@
 "use strict"
 
-const elements = {
+async function getRandomImage() {
+    const client_id = "";
+    const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
+    try {
+        const response = await fetch(endpoint);
+        const returnedData = await response.json();
+        const receivedPhotoUrl = returnedData.urls.regular;
+
+        const imgDiv = document.querySelector(".background-img");
+        imgDiv.style.backgroundImage = `url("${receivedPhotoUrl}")`;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+getRandomImage(); 
+
+/* const elements = {
     quote: document.getElementById("quote"),
     author: document.getElementById("author"),
 };
@@ -42,4 +59,4 @@ function loopThroughQuotes() {
     }, 3000);
 }
 
-setTimeout(loopThroughQuotes, 3000);
+setTimeout(loopThroughQuotes, 3000); */
