d8a012aa1fb573eef57912a097da00b283cd5025
wslider <wslider2000@gmail.com>
2025-10-17 14:29:19 -0400

    cors options and dotenv

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,14 @@
 "use strict"
 
 async function getRandomImage() {
-    const client_id = "";
-    const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
+    const endpoint = "http://localhost:8080/api/v1/getRandomImage";
     try {
         const response = await fetch(endpoint);
         const returnedData = await response.json();
-        const receivedPhotoUrl = returnedData.urls.regular;
+        const receivedPhotoUrl = returnedData.data;
 
         const imgDiv = document.querySelector(".background-img");
-        imgDiv.style.backgroundImage = `url("${receivedPhotoUrl}")`;
+        imgDiv.style.backgroundImage = `url(${receivedPhotoUrl})`;
     } catch (error) {
         console.error(error);
     }
--- server.js
+++ server.js
@@ -4,13 +4,38 @@ const express = require("express");
 const app = express();
 
 const port = 8080;
+require("dotenv").config();
+const cors = require("cors"); 
+const corsOptions = {
+    origin: `http://localhost:${port}`
+}
+app.use(cors(corsOptions));
 
 app.use(express.static("./public"))
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+
+app.use("/api/v1/getRandomImage", async (request, response) => {
+    response.status(200).json({
+        status: 200,
+        data: await getRandomImage(),
+    });
+});
 
 app.listen(port, () => {
     console.log(`Server is running http://localhost:${port}`);
     console.log("Press Ctrl+C to end this process.");
 });
 
+async function getRandomImage() {
+    const endpoint = `https://api.unsplash.com/photos/random/?client_id=${process.env.CLIENT_ID}`;
+    try {
+        const response = await fetch(endpoint);
+        const returnedData = await response.json();
+        const receivedPhotoUrl = returnedData.urls.regular;
+
+        return receivedPhotoUrl;
+    } catch (error) {
+        console.error(error);
+    }
+}
